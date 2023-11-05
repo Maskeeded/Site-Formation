f33cbb51ccd9eb20e7da5930c56ce56b43317979
Maskeeded <maskeeded@gmail.com>
2023-11-05 11:08:25 +0100

    finished interview site and other fixes

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -14,7 +14,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -22,3 +21,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
